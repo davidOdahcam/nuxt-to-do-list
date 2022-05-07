a1e0f062acf9d0e295a7bc6e2427643dab7974d2
David dos Santos Machado <david.desenvolvedor@gmail.com>
2022-05-06 23:13:24 -0300

    Creating index page

--- store/index.js
+++ store/index.js
@@ -1,42 +1,15 @@
-export const state = () => {
-  tasks: [
-    {
-      title: "Get milk",
-      done: false
-    },
-    {
-      title: "Get milk",
-      done: true
-    },
-    {
-      title: "Get milk",
-      done: false
-    },
-    {
-      title: "Get milk",
-      done: true
-    },
-    {
-      title: "Get milk",
-      done: true
-    },
-    {
-      title: "Get milk",
-      done: false
-    },
-  ]
-}
+export const state = () => ({
+  tasks: [],
+});
 
 export const mutations = {
   ADD_TASK(state, task) {
-    state.tasks = [{ title: task, done: false }, ...state.tasks];
+    state.tasks = [{ content: task, done: false }, ...state.tasks];
   },
-
   REMOVE_TASK(state, task) {
     state.tasks.splice(state.tasks.indexOf(task), 1);
   },
-
   TOGGLE_TASK(state, task) {
     task.done = !task.done;
   },
-}
+};
